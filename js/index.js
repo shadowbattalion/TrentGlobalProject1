60482f98efcc812f2314fc48d20fe1e0d7ecbb52
shadowbattalion <hafizspaceumar@gmail.com>
2021-09-15 12:54:22 +0000

    type issue resolved. Moving on to layers

--- js/index.js
+++ js/index.js
@@ -2,14 +2,18 @@ async function main(){
     function init(){// in JS init( can only exist in main()
       
         let map = initMap()//leaflet api, creates the map
-        let car_park_layer = L.layerGroup()
+        let c_car_park_layer = L.layerGroup()
+        let y_car_park_layer = L.layerGroup()
+        let h_car_park_layer = L.layerGroup()
+        let main_car_park_layer = L.layerGroup()
         // let places_layer = L.layerGroup()
         // places_layer.addTo(map)
+        let layers = [c_car_park_layer, y_car_park_layer, h_car_park_layer, main_car_park_layer]
         window.addEventListener('DOMContentLoaded', async function(){
             //carParkLayer
             let car_park_list = await findCarPark()
             let car_park_status_list= await carParkStatus()
-            generateCarParkLayer(car_park_list,car_park_status_list,car_park_layer, map)
+            generateCarParkLayer(car_park_list,car_park_status_list,layers, map)
             
 
         })
--- js/view/car_park_view.js
+++ js/view/car_park_view.js
@@ -1,6 +1,6 @@
-async function generateCarParkLayer(car_park_list,car_park_status_list,car_park_layer, map){
+async function generateCarParkLayer(car_park_list,car_park_status_list, layers, map){
 
-    car_park_layer.clearLayers()
+    layers[3].clearLayers()
     // console.log(car_park_list.result.records)
 
     let carParkMarkerCluster = L.markerClusterGroup()
@@ -9,28 +9,18 @@ async function generateCarParkLayer(car_park_list,car_park_status_list,car_park_
         let status = resolve_carpark_number(car_park_status_list, car_park.car_park_no)
         
         if(status){
-         
-            for (let i = 0;  i < status.carpark_info.length; i++){
             
-                let display_status =  {
-                                        "available_lots" : "No information found",
-                                        "total_lots": "No information found",
-                                        "occupied_lots": "No information found",
-                                        "percentage_lots":null,
-                                        "last_updated": null,
-                                        "car_park_type":"No information found"
-                                        }
-                if (status){
+            for (let i = 0;  i < status.carpark_info.length; i++){ // loops through the types of car park. some car parks have three types.
             
-                    display_status = {
-                                        "available_lots":parseInt(status.carpark_info[i].lots_available), 
-                                        "total_lots":parseInt(status.carpark_info[i].total_lots),
-                                        "occupied_lots": parseInt(status.carpark_info[i].total_lots) - parseInt(status.carpark_info[i].lots_available),
-                                        "percentage_lots":((status.carpark_info[i].total_lots-status.carpark_info[i].lots_available)/status.carpark_info[i].total_lots)*100,
-                                        "last_updated":status.update_datetime,
-                                        "car_park_type":status.carpark_info[i].lot_type
-                                        }
-                }
+                 display_status = {
+                                    "available_lots":parseInt(status.carpark_info[i].lots_available), 
+                                    "total_lots":parseInt(status.carpark_info[i].total_lots),
+                                    "occupied_lots": parseInt(status.carpark_info[i].total_lots) - parseInt(status.carpark_info[i].lots_available),
+                                    "percentage_lots":((status.carpark_info[i].total_lots-status.carpark_info[i].lots_available)/status.carpark_info[i].total_lots)*100,
+                                    "last_updated":status.update_datetime,
+                                    "car_park_type":status.carpark_info[i].lot_type
+                                    }
+                
 
                 let lat = car_park.x_coord
                 let lng = car_park.y_coord
@@ -46,23 +36,47 @@ async function generateCarParkLayer(car_park_list,car_park_status_list,car_park_
 
 
 
-                if (display_status["car_park_type"]==="H"){
-                    console.log(display_status["car_park_type"]==="H")
-                    let carParkMarker = L.marker(svy21ToWgs84(lng, lat),{"icon":modified_icon})
+                
+                let carParkMarker = L.marker(svy21ToWgs84(lng, lat),{"icon":modified_icon})
 
-                    carParkMarker.bindPopup(`<h3 id="carpark_number">${car_park.address}</h3> <p> Carpark Number: ${car_park.car_park_no} <br> Available Lots: ${display_status["available_lots"]} <br> Occupied Lots: ${display_status["occupied_lots"]} <br> Total Lots: ${display_status["total_lots"]} <br> Last updated: ${last_updated_duration(display_status["last_updated"])} <br> Car Park Type: ${display_status["car_park_type"]}  <br> <button onclick="refresh()">Refresh</button> </p>`) //Check if refresh-btn attr should be class or id
+                carParkMarker.bindPopup(`<h3 id="carpark_number">${car_park.address}</h3> <p> Carpark Number: ${car_park.car_park_no} <br> Available Lots: ${display_status["available_lots"]} <br> Occupied Lots: ${display_status["occupied_lots"]} <br> Total Lots: ${display_status["total_lots"]} <br> Last updated: ${last_updated_duration(display_status["last_updated"])} <br> Car Park Type: ${display_status["car_park_type"]}  <br> <button onclick="refresh()">Refresh</button> </p>`) //Check if refresh-btn attr should be class or id
                 
 
-                    carParkMarker.addTo(carParkMarkerCluster)
-                }
+                carParkMarker.addTo(carParkMarkerCluster)
+                
             }
         }
     }
 
-    carParkMarkerCluster.addTo(car_park_layer)
+    carParkMarkerCluster.addTo(layers[3])
     
-    car_park_layer.addTo(map)
+    
+    layers[3].addTo(map)
 
+}
+
+
+
+
+function resolve_carpark_number(car_park_status_list, car_park_no){
+    
+    let chosen_carpak_status={}
+
+    for (let status of car_park_status_list.items[0].carpark_data){
+        
+        // console.log(status)
+        if(status.carpark_number === car_park_no){
+            chosen_carpak_status = status
+            break
+        } else {
+
+            chosen_carpak_status = null
+
+        }
+        
+    }
+    // console.log(chosen_carpak_status)
+    return chosen_carpak_status
 }
 
 
@@ -81,6 +95,73 @@ function last_updated_duration(datetime){
         return `${duration} mins ago`
     }
     return "No information"
+
+}
+
+
+
+
+
+function percentage_to_icon(percentage){
+
+    let icon = ""
+
+ 
+
+    if(percentage===100){
+
+        icon="images/full_100.png"
+
+    }else if(percentage >= 90 && percentage < 100){
+
+        icon="images/full_90.png"
+
+    }else if(percentage >= 80 && percentage < 90){
+
+        icon="images/full_80.png"
+            
+    }else if(percentage >= 70 && percentage < 80){
+
+        icon="images/full_70.png"
+            
+    }else if(percentage >= 60 && percentage < 70){
+
+        icon="images/full_60.png"
+            
+    }else if(percentage >= 50 && percentage < 60){
+
+        icon="images/full_50.png"
+            
+    }else if(percentage >= 40 && percentage < 50){
+
+        icon="images/full_40.png"
+            
+    }else if(percentage >= 30 && percentage < 40){
+
+        icon="images/full_30.png"
+            
+    }else if(percentage >= 20 && percentage < 30){
+
+        icon="images/full_20.png"
+            
+    }else if(percentage >= 10 && percentage < 20){
+
+        icon="images/full_10.png"
+            
+    }else if(percentage >= 0 && percentage < 10){
+
+        icon="images/full_0.png"
+            
+    }else{
+
+        icon="images/full_null.png" // No info because certain car parks returns a negative percentage or a NaN
+
+    }
+
+    
+
+    return icon
+
 
 }
 
