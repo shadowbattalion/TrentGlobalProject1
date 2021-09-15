c61414968ffe1dc01b0477b6095e9a2b04cf2029
shadowbattalion <hafizspaceumar@gmail.com>
2021-09-15 11:03:53 +0000

    carpark type issue

--- js/view/car_park_view.js
+++ js/view/car_park_view.js
@@ -7,51 +7,56 @@ async function generateCarParkLayer(car_park_list,car_park_status_list,car_park_
     for(let car_park of car_park_list.result.records){
        
         let status = resolve_carpark_number(car_park_status_list, car_park.car_park_no)
-        // console.log(status)
         
-        let display_status =  {
-                                "available_lots" : "No information found",
-                                "total_lots": "No information found",
-                                "occupied_lots": "No information found",
-                                "percentage_lots":null,
-                                "last_updated": null
-                                }
-        if (status){
-       
-            display_status = {
-                                "available_lots":parseInt(status.carpark_info[0].lots_available), 
-                                "total_lots":parseInt(status.carpark_info[0].total_lots),
-                                "occupied_lots": parseInt(status.carpark_info[0].total_lots) - parseInt(status.carpark_info[0].lots_available),
-                                "percentage_lots":((status.carpark_info[0].total_lots-status.carpark_info[0].lots_available)/status.carpark_info[0].total_lots)*100,
-                                "last_updated":status.update_datetime
-                                }
+        if(status){
+         
+            for (let i = 0;  i < status.carpark_info.length; i++){
+            
+                let display_status =  {
+                                        "available_lots" : "No information found",
+                                        "total_lots": "No information found",
+                                        "occupied_lots": "No information found",
+                                        "percentage_lots":null,
+                                        "last_updated": null,
+                                        "car_park_type":"No information found"
+                                        }
+                if (status){
+            
+                    display_status = {
+                                        "available_lots":parseInt(status.carpark_info[i].lots_available), 
+                                        "total_lots":parseInt(status.carpark_info[i].total_lots),
+                                        "occupied_lots": parseInt(status.carpark_info[i].total_lots) - parseInt(status.carpark_info[i].lots_available),
+                                        "percentage_lots":((status.carpark_info[i].total_lots-status.carpark_info[i].lots_available)/status.carpark_info[i].total_lots)*100,
+                                        "last_updated":status.update_datetime,
+                                        "car_park_type":status.carpark_info[i].lot_type
+                                        }
+                }
+
+                let lat = car_park.x_coord
+                let lng = car_park.y_coord
+            
+                // console.log(display_status["percentage_lots"])
+                var modified_icon = L.icon({
+                        iconUrl:      percentage_to_icon(display_status["percentage_lots"]),
+                        iconSize:     [80, 80],
+                        iconAnchor:   [27, 75],
+                        popupAnchor:  [0, -60]
+
+                    });
+
+
+
+                if (display_status["car_park_type"]==="H"){
+                    console.log(display_status["car_park_type"]==="H")
+                    let carParkMarker = L.marker(svy21ToWgs84(lng, lat),{"icon":modified_icon})
+
+                    carParkMarker.bindPopup(`<h3 id="carpark_number">${car_park.address}</h3> <p> Carpark Number: ${car_park.car_park_no} <br> Available Lots: ${display_status["available_lots"]} <br> Occupied Lots: ${display_status["occupied_lots"]} <br> Total Lots: ${display_status["total_lots"]} <br> Last updated: ${last_updated_duration(display_status["last_updated"])} <br> Car Park Type: ${display_status["car_park_type"]}  <br> <button onclick="refresh()">Refresh</button> </p>`) //Check if refresh-btn attr should be class or id
+                
+
+                    carParkMarker.addTo(carParkMarkerCluster)
+                }
+            }
         }
-
-        let lat = car_park.x_coord
-        let lng = car_park.y_coord
-     
-        // console.log(display_status["percentage_lots"])
-        var modified_icon = L.icon({
-                iconUrl:      percentage_to_icon(display_status["percentage_lots"]),
-                iconSize:     [80, 80],
-                shadowSize:   [0, 0],
-                iconAnchor:   [27, 75],
-                shadowAnchor: [0, 0],
-                popupAnchor:  [0, -60]
-
-            });
-
-
-
-
-        let carParkMarker = L.marker(svy21ToWgs84(lng, lat),{"icon":modified_icon})
-
-        carParkMarker.bindPopup(`<h3 id="carpark_number">${car_park.address}</h3> <p> Carpark Number: ${car_park.car_park_no} <br> Available Lots: ${display_status["available_lots"]} <br> Occupied Lots: ${display_status["occupied_lots"]} <br> Total Lots: ${display_status["total_lots"]} <br> Last updated: ${last_updated_duration(display_status["last_updated"])} <br> <button onclick="refresh()">Refresh</button> </p>`) //Check if refresh-btn attr should be class or id
-       
-
-        carParkMarker.addTo(carParkMarkerCluster)
-      
-
     }
 
     carParkMarkerCluster.addTo(car_park_layer)
