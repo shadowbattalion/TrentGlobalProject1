d1a997eb07b3d39ef421fcd09812c20ce21a85c8
shadowbattalion <hafizspaceumar@gmail.com>
2021-09-16 14:19:22 +0000

    fixed refresh with intervals

--- js/index.js
+++ js/index.js
@@ -9,7 +9,13 @@ async function main(){
             //carParkLayer
             let car_park_list = await findCarPark()
             let car_park_status_list= await carParkStatus()
-            generateCarParkLayer(car_park_list,car_park_status_list,car_park_layer, map)
+            generateCarParkLayer(car_park_list, car_park_status_list, car_park_layer, map)
+            let i=0
+            setInterval(async function(){
+                console.log(i)
+                i++
+                let car_park_status_list= await carParkStatus()
+                generateCarParkLayer(car_park_list, car_park_status_list, car_park_layer, map)}, 60000)
             
 
         })
--- js/view/car_park_view.js
+++ js/view/car_park_view.js
@@ -1,18 +1,20 @@
-async function generateCarParkLayer(car_park_list,car_park_status_list,car_park_layer, map){
+async function generateCarParkLayer(car_park_list, car_park_status_list, car_park_layer, map){
 
+    
+    
     car_park_layer.clearLayers()
     // console.log(car_park_list.result.records)
 
     let marker_cluster = L.markerClusterGroup()
     for(let car_park of car_park_list.result.records){
-       
-        let status = resolve_carpark_number(car_park_status_list, car_park.car_park_no)
         
+        let status = resolve_carpark_number(car_park_status_list, car_park.car_park_no)
+            
         if(status){
-            
+                
             for (let i = 0;  i < status.carpark_info.length; i++){ // loops through the types of car park. some car parks have three types.
-            
-                 display_status = {
+                
+                display_status = {
                                     "available_lots":parseInt(status.carpark_info[i].lots_available), 
                                     "total_lots":parseInt(status.carpark_info[i].total_lots),
                                     "occupied_lots": parseInt(status.carpark_info[i].total_lots) - parseInt(status.carpark_info[i].lots_available),
@@ -20,11 +22,11 @@ async function generateCarParkLayer(car_park_list,car_park_status_list,car_park_
                                     "last_updated":status.update_datetime,
                                     "car_park_type":status.carpark_info[i].lot_type
                                     }
-                
+                    
 
                 let lat = car_park.x_coord
                 let lng = car_park.y_coord
-            
+                
                 // console.log(display_status["percentage_lots"])
                 var modified_icon = L.icon({
                         iconUrl:      percentage_to_icon(display_status["percentage_lots"]),
@@ -36,25 +38,24 @@ async function generateCarParkLayer(car_park_list,car_park_status_list,car_park_
 
 
 
-                
+                    
                 let marker = L.marker(svy21ToWgs84(lng, lat),{"icon":modified_icon})
 
                 marker.bindPopup(`<h3 id="carpark_number">${car_park.address}</h3> <p> Carpark Number: ${car_park.car_park_no} <br> Available Lots: ${display_status["available_lots"]} <br> Occupied Lots: ${display_status["occupied_lots"]} <br> Total Lots: ${display_status["total_lots"]} <br> Last updated: ${last_updated_duration(display_status["last_updated"])} <br> Car Park Type: ${display_status["car_park_type"]}  <br> <button onclick="refresh()">Refresh</button> </p>`) //Check if refresh-btn attr should be class or id
-                
+                    
 
                 marker.addTo(marker_cluster)
-                
+                    
             }
         }
-    }
 
     marker_cluster.addTo(car_park_layer)
-    
+        
     car_park_layer.addTo(map)
+       
+    }   
 
 }
-
-
 
 
 function resolve_carpark_number(car_park_status_list, car_park_no){
@@ -163,17 +164,3 @@ function percentage_to_icon(percentage){
 
 
 }
-
-
-
-// function refresh(){
-
-//     let status = await carParkStatus(hdb_json_info.car_park_no)
-//     carParkMarker.bindPopup(`<p>${hdb_json_info.car_park_no} ${hdb_json_info.address} ${status}</p><button class="refresh-btn">Refresh</button>`) //Check if refresh-btn attr should be class or id
-    
-//     let carpark_number = document.querySelector(".carpark_number")
-//     console.log("test")
-//     findCarPark(carParkLayer)
-    
-
-//     }
