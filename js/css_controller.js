2da6c880f051bf441a0ab255887fe38808528570
shadowbattalion <hafizspaceumar@gmail.com>
2021-09-22 06:09:37 +0000

    working on the search group animation

---
+++ js/css_controller.js
@@ -0,0 +1,10 @@
+let search_icon = document.querySelector("#search-icon")
+
+search_icon.addEventListener('click', function(){
+
+    let search_group = document.querySelector("#search-group")
+    
+
+
+
+})
