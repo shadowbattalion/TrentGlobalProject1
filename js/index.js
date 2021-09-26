75f4a1af8a15e356f93a525d1352ca2724ebb332
shadowbattalion <hafizspaceumar@gmail.com>
2021-09-26 07:39:26 +0000

    almost done with welcome banner

--- js/index.js
+++ js/index.js
@@ -27,7 +27,29 @@ async function main(){
 
         })
 
-      
+        let search_icon = document.querySelector("#search-icon")
+
+        search_icon.addEventListener('click', function(){
+
+            let banner = document.querySelector("#banner")
+            banner.classList.add("hide-banner")
+            let inner_banner = document.querySelector("#inner-banner")
+            inner_banner.classList.add("hide-banner")
+            console.log(banner.classList)
+            let search_group = document.querySelector(".search-group")
+            search_group.classList.toggle("search-group-expand")
+            let search_button =  document.querySelector(".search-btn")
+            search_button.classList.toggle("search-btn-expand")
+            let search_input =  document.querySelector(".search-input")
+            search_input.classList.toggle("search-input-expand")
+            let search_results =  document.querySelector(".search-results")
+            search_results.classList.toggle("search-results-expand")
+            let search_container =  document.querySelector(".search-container")
+            search_container.classList.toggle("search-container-expand")
+
+
+
+        })
          
           
         }
